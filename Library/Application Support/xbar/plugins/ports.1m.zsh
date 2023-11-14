61241a06502b63fcd616c1fb1f44da7d61f30e55
Chuancong Gao <chuanconggao@users.noreply.github.com>
2023-11-14 12:56:08 -0800

    Update xbar plugin for open ports
    To separate well-known ports by macOS and highlight ports by others

--- Library/Application Support/xbar/plugins/ports.1m.zsh
+++ Library/Application Support/xbar/plugins/ports.1m.zsh
@@ -6,8 +6,34 @@ source ~/.zprofile
 OPEN_PORTS=$(nmap --open localhost | grep "open" | awk '{print $1,$3}')
 OPEN_PORTS_NUM=$(echo -n "$OPEN_PORTS" | grep -c "")
 
-echo "Ports: $OPEN_PORTS_NUM"
+# - 631 for internet printing protocol
+# - 5000 for universal plug-and-play
+# - 7000 for AirPlay receiver
+MACOS_PORTS_REGEX="^(?:631|5000|7000)/tcp "
+OTHERS_OPEN_PORTS_NUM=$(echo -n "$OPEN_PORTS" | grep -c -v -P "$MACOS_PORTS_REGEX")
+
+if [[ $OTHERS_OPEN_PORTS_NUM -gt 0 ]]; then
+    echo "Ports: $OPEN_PORTS_NUM | color=yellow"
+else
+    echo "Ports: $OPEN_PORTS_NUM"
+fi
 
 echo "---"
 
-echo "$OPEN_PORTS"
+echo "By macOS"
+
+echo "---"
+
+echo -n "$OPEN_PORTS" | grep -P "$MACOS_PORTS_REGEX"
+
+echo "---"
+
+if [[ $OTHERS_OPEN_PORTS_NUM -gt 0 ]]; then
+    echo "By Others | color=yellow"
+
+    echo "---"
+
+    echo -n "$OPEN_PORTS" | grep -v -P "$MACOS_PORTS_REGEX"
+
+    echo "---"
+fi
