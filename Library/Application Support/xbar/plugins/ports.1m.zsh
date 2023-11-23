43b2cec63d88819845dd2e8bc8a448eb0164be21
Chuancong Gao <chuanconggao@users.noreply.github.com>
2023-11-22 18:37:57 -0800

    Update xbar plugin for open ports
    Switch to `lsof` instead of `nmap`

--- Library/Application Support/xbar/plugins/ports.1m.zsh
+++ Library/Application Support/xbar/plugins/ports.1m.zsh
@@ -3,13 +3,13 @@
 source /etc/zprofile
 source ~/.zprofile
 
-OPEN_PORTS=$(nmap --open localhost | grep "open" | awk '{print $1,$3}')
+OPEN_PORTS=$(lsof +c 100 -n -P -iTCP -sTCP:LISTEN | tail -n +2 | awk '{print $9,$1}' | grep -v -P '\b127.0.0.1:' | sed 's/^*://' | sort -n -u)
 OPEN_PORTS_NUM=$(echo -n "$OPEN_PORTS" | grep -c "")
 
 # - 631 for internet printing protocol
 # - 5000 for universal plug-and-play
 # - 7000 for AirPlay receiver
-MACOS_PORTS_REGEX="^(?:631|5000|7000)/tcp "
+MACOS_PORTS_REGEX="^(?:[57]000 ControlCenter|[0-9]+ rapportd)$"
 OTHERS_OPEN_PORTS_NUM=$(echo -n "$OPEN_PORTS" | grep -c -v -P "$MACOS_PORTS_REGEX")
 
 if [[ $OTHERS_OPEN_PORTS_NUM -gt 0 ]]; then
