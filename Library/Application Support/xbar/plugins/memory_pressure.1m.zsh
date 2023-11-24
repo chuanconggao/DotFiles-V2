0b77ac4e07bb6ecde617d116a05d7edc90906c9f
Chuancong Gao <chuanconggao@users.noreply.github.com>
2023-11-24 06:52:56 -0800

    Update xbar plugin for memory pressure
    Remove noisy notification

--- Library/Application Support/xbar/plugins/memory_pressure.1m.zsh
+++ Library/Application Support/xbar/plugins/memory_pressure.1m.zsh
@@ -10,12 +10,8 @@ if [[ $MEMORY_PRESSURE -le 60 ]]; then
     COLOR="green"
 elif [[ $MEMORY_PRESSURE -le 80 ]]; then
     COLOR="yellow"
-
-    terminal-notifier -title "Memory Pressure" -message "Warning: $MEMORY_PRESSURE% memory pressure." -group "xbar/memory_pressure" > /dev/null
 else
     COLOR="red"
-
-    terminal-notifier -title "Memory Pressure" -message "Critical: $MEMORY_PRESSURE% memory pressure." -group "xbar/memory_pressure" > /dev/null
 fi
 
 echo "Mem: $MEMORY_PRESSURE% | color=$COLOR"
