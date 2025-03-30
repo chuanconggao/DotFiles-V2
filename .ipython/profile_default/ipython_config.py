3ef5bef6299644b70d7f9de23cdde85318c27d2e
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-03-29 18:11:00 -0700

    Add ipython configuration

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,11 @@
+c.InteractiveShell.enable_tip = False
+
+c.InteractiveShellApp.exec_lines = [
+    "import json",
+    "import math",
+    "import os",
+    "import pathlib",
+]
+
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.editor = "code"
