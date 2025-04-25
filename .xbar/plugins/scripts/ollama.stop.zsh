2730d41f374e80d6ec21ef47391b19f65c115afb
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-04-24 19:04:27 -0700

    Update xbar plugin for stopping Ollama

--- .xbar/plugins/scripts/ollama.stop.zsh
+++ .xbar/plugins/scripts/ollama.stop.zsh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
 
+ollama ps | tail -n +2 | awk '{print $1}' | xargs ollama stop
+# Wait until all models are stopped
+sleep 5
+
 PID=$(ps -A | awk '{print $1,$4}' | grep ollama | awk '{print $1}')
 kill -INT "$PID" && open -g "swiftbar://refreshplugin?name=services"
