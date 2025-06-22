8b93dfadcb290b5ef7d51f08e98a01063cf8a47a
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-06-21 20:52:48 -0700

    Update xbar plugin for starting LocalStack

--- .xbar/plugins/scripts/localstack.start.zsh
+++ .xbar/plugins/scripts/localstack.start.zsh
@@ -1,3 +1,11 @@
 #!/usr/bin/env zsh
 
+if (! docker ps > /dev/null 2> /dev/null); then
+    echo "Starting Docker first..."
+
+    open -a "Docker.app"
+    # Wait until Docker is running
+    sleep 5
+fi
+
 localstack start --detached && open -g "swiftbar://refreshplugin?name=services"
